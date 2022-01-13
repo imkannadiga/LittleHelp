d0f79ad06d629ae84f79e68ef20006e05bf38eb5
kannadiga <alberteinstine974@gmail.com>
2022-01-13 21:43:21 +0530

    Added the main activity

--- app/src/main/java/com/example/littlehelp/MainActivity.java
+++ app/src/main/java/com/example/littlehelp/MainActivity.java
@@ -1,13 +1,6 @@
 package com.example.littlehelp;
 
-import androidx.appcompat.app.AppCompatActivity;
-import android.os.Bundle;
+import android.app.Activity;
 
-public class MainActivity extends AppCompatActivity {
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
+public class MainActivity extends Activity {
 }
