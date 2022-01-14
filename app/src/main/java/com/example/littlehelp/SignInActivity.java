7bf43850034f7ab0235bfa7a833eceff774efa9e
rahulramesh2602 <rahulramesh2602@gmail.com>
2022-01-14 22:17:49 +0530

    Addede auth functionality

--- app/src/main/java/com/example/littlehelp/SignInActivity.java
+++ app/src/main/java/com/example/littlehelp/SignInActivity.java
@@ -2,14 +2,40 @@ package com.example.littlehelp;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.EditText;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.amplifyframework.AmplifyException;
+import com.amplifyframework.auth.cognito.AWSCognitoAuthPlugin;
+import com.amplifyframework.core.Amplify;
 
 public class SignInActivity extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        EditText email= findViewById(R.id.Signin_Emailid);
+        EditText password= findViewById(R.id.Signin_Password);
+
+        try {
+
+            Amplify.configure(getApplicationContext());
+            Amplify.addPlugin(new AWSCognitoAuthPlugin());
+
+
+            Log.i("MyAmplifyApp", "Initialized Amplify");
+        } catch (AmplifyException error) {
+            Log.e("MyAmplifyApp", "Could not initialize Amplify", error);
+        }
+
+        Amplify.Auth.signIn(
+                email.getText().toString(),
+                password.getText().toString(),
+                result -> Log.i("AuthQuickstart", result.isSignInComplete() ? "Sign in succeeded" : "Sign in not complete"),
+                error -> Log.e("AuthQuickstart", error.toString())
+        );
     }
 }
